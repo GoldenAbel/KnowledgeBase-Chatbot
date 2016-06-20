987e39296cfe10868404ed62cd00ba53576e490f
Dr_I_m_possible <ameya.prabhu@research.iiit.ac.in>
2016-06-21 02:53:39 +0530

    Minor final changes

--- training.sh
+++ training.sh
@@ -1,3 +1,12 @@
 cd src/training/
-python train_synopsis.pyc
+echo 'Which system would you like to start?'
+echo 'Press 1 to start Reddit based system'
+echo 'Press 2 to start Synopsis based system'
+
+read vari
+case $vari in
+  1|Reddit) python train_reddit.pyc ;;
+  2|Synopsis) python train_synopsis.pyc ;;
+  *) echo 'Incorrect input!';;
+esac
 cd ../../
